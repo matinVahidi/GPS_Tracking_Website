62c0503ecc621252ac0919f8562926fb61986c26
matinVahidi <matinvahidi83@gmail.com>
2025-08-30 15:24:43 +0330

    I don't know maybe this works

--- server.js
+++ server.js
@@ -48,6 +48,6 @@ app.get('/data', (req, res) => {
 app.use(express.static(path.join(__dirname, 'public')));
 
 // Start server
-app.listen(PORT, '213.233.184.186', () => {
+app.listen(PORT, '0.0.0.0', () => {
   console.log(`Server is running on http://213.233.184.186:${PORT}`);
 });
